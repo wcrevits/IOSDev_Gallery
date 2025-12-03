a0cda528667761ba0226641115962acbc285b253
Wout Crevits <woutcrevits@LAPTOP-KATO.vivesnet.be>
2025-12-03 09:08:57 +0100

    first commit

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Wout Crevits on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
